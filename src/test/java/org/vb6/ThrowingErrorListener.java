cbf2e5d26ff1988790992c9b196cc2be87e1f3cb
uwol <u.wol@wwu.de>
2015-03-02 22:40:49 +0100

    comments

--- src/test/java/org/vb6/ThrowingErrorListener.java
+++ src/test/java/org/vb6/ThrowingErrorListener.java
@@ -1,20 +1,20 @@
 /*
 Copyright (C) 2014 u.wol@wwu.de
 
-This file is part of vb6transform.
+This file is part of vb6grammar.
 
-vb6transform is free software: you can redistribute it and/or modify
+vb6grammar is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-vb6transform is distributed in the hope that it will be useful,
+vb6grammar is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with vb6transform. If not, see <http://www.gnu.org/licenses/>.
+along with vb6grammar. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.vb6;
--- src/test/java/org/vb6/runner/VbParseTestRunner.java
+++ src/test/java/org/vb6/runner/VbParseTestRunner.java
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2014 u.wol@wwu.de
+
+This file is part of vb6grammar.
+
+vb6grammar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+vb6grammar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with vb6grammar. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.vb6.runner;
 
 import java.io.File;
